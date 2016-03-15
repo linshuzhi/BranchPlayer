70ee562c5086b262c6d899410c2327859c935d14
qq_33187999 <540096836@qq.com>
2016-03-15 20:11:36 +0800

    first ios

---
+++ BranchPlayer_ios/BranchPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BranchPlayer
+//
+//  Created by LinShulong on 16/3/15.
+//  Copyright © 2016年 LinShulong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BranchPlayer_ios/BranchPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BranchPlayer
+//
+//  Created by LinShulong on 16/3/15.
+//  Copyright © 2016年 LinShulong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
